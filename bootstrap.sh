afe2c055856feb385ee78ec6881bf28643a48381
Dodda Srinivasan <srinivasan.munna@gmail.com>
2016-12-21 16:57:59 +0530

    trying to install ansible on shell provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+# Install Ansible
+sudo rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo yum install -y ansible
+
+# Install Apache Web Server
 sudo yum install -y httpd mod_ssl
+
+# Set Html path
 sudo rm -rf /var/www/html
 sudo ln -fs /srini/html /var/www
+
+# Start Apache server
 sudo /usr/sbin/apachectl start
