8f64aecadbf3b99c98d729b1f9c4514325d024ef
Dodda Srinivasan <srinivasan.munna@gmail.com>
2016-12-23 18:36:15 +0530

    Installing apache and starting from ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,15 @@
 sudo yum install -y epel-release
 sudo yum install -y ansible
 
-# Install Apache Web Server
-sudo yum install -y httpd mod_ssl
+# Change to Ansible Directory
+cd /srini/ansible
+
+# Install Web Components Setup
+ansible-playbook playbooks/webserver.yml
 
 # Set Html path
 sudo rm -rf /var/www/html
 sudo ln -fs /srini/html /var/www
 
-# Start Apache server
-sudo /usr/sbin/apachectl start
+# Install Load Balancer (Nginx)
+ansible-playbook playbooks/loadbalancer.yml
